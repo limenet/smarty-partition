b7e46778704fc3ca6ccf00eeb580b3ddb9799211
Linus Metzler <linus.metzler@gmail.com>
2015-09-27 10:37:22 +0200

    bugfix for empty array

--- function.partition.php
+++ function.partition.php
@@ -1,14 +1,13 @@
 <?php
-
 function smarty_function_partition($params, $template) {
-    $listlen   = count( $params['array'] );
-    $partlen   = floor( $listlen / $params['size'] );
+    $listlen   = count($params['array']);
+    $partlen   = floor($listlen / $params['size']);
     $partrem   = $listlen % $params['size'];
     $partition = [];
     $mark      = 0;
     for ($px = 0; $px < $params['size']; $px++) {
         $incr = ($px < $partrem) ? $partlen + 1 : $partlen;
-        $partition[$px] = array_slice( $params['array'], $mark, $incr );
+        $partition[$px] = array_slice($params['array'] ?: [], $mark, $incr);
         $mark += $incr;
     }
     $template->assign($params['name'], $partition);
