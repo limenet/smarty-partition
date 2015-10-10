a80ed67cb97fd5aadb128c10631e57b5b0adc92d
Linus Metzler <linus.metzler@gmail.com>
2015-10-10 10:51:23 +0200

    added StyleCI

--- function.partition.php
+++ function.partition.php
@@ -1,9 +1,11 @@
 <?php
 /**
  * This function partitions an array into n subarrays which can be used for column layouts
- * @param  array $params    An array consisting of the keys array, size, and name; name is the name of the output array, size is the number of columns, array is the array to be partitioned
- * @param  Smarty_Internal_Template $template Used by Smarty internally
- * @return array            Returns the partitoned array
+ *
+ * @param  array $params                An array consisting of the keys array, size, and name; name is the name of the output array, size is the number of columns, array is the array to be partitioned
+ * @param  Smarty_Internal_Template     $template Used by Smarty internally
+ *
+ * @return array Returns the partitoned array
  */
 
 function smarty_function_partition($params, Smarty_Internal_Template $template = null) {
@@ -16,17 +18,17 @@ function smarty_function_partition($params, Smarty_Internal_Template $template =
         }
     }
 
-    $listlen   = count($params['array']);
-    $partlen   = floor($listlen / $params['size']);
-    $partrem   = $listlen % $params['size'];
+    $listlen = count($params['array']);
+    $partlen = floor($listlen / $params['size']);
+    $partrem = $listlen % $params['size'];
     $partition = [];
-    $mark      = 0;
+    $mark = 0;
+
     for ($px = 0; $px < $params['size']; $px++) {
         $incr = ($px < $partrem) ? $partlen + 1 : $partlen;
         $partition[$px] = array_slice($params['array'] ?: [], $mark, $incr);
         $mark += $incr;
     }
-
 
     if (!empty($template)) {
         $template->assign($params['name'], $partition);
--- test.php
+++ test.php
@@ -2,8 +2,10 @@
 
 require_once 'function.partition.php';
 
-class Test extends PHPUnit_Framework_TestCase {
-	public function wrapper($a) {
+class Test extends PHPUnit_Framework_TestCase
+{
+	public function wrapper($a)
+    {
 		return smarty_function_partition($a, null);
 	}
 
@@ -34,16 +36,16 @@ class Test extends PHPUnit_Framework_TestCase {
 
 	public function testPerfectFit()
     {
-        $this->assertEquals(['test' => [[1,2,3],[4,5,6],[7,8,9]]], $this->wrapper(['array' => range(1,9), 'name' => 'test', 'size' => 3]));
+        $this->assertEquals(['test' => [[1,2,3],[4,5,6],[7,8,9]]], $this->wrapper(['array' => range(1, 9), 'name' => 'test', 'size' => 3]));
     }
 
 	public function testPerfectOneShort()
     {
-        $this->assertEquals(['test' => [[1,2,3],[4,5,6],[7,8]]], $this->wrapper(['array' => range(1,8), 'name' => 'test', 'size' => 3]));
+        $this->assertEquals(['test' => [[1,2,3],[4,5,6],[7,8]]], $this->wrapper(['array' => range(1, 8), 'name' => 'test', 'size' => 3]));
     }
 
 	public function testPerfectOneOver()
     {
-        $this->assertEquals(['test' => [[1,2,3,4],[5,6,7],[8,9,10]]], $this->wrapper(['array' => range(1,10), 'name' => 'test', 'size' => 3]));
+        $this->assertEquals(['test' => [[1,2,3,4],[5,6,7],[8,9,10]]], $this->wrapper(['array' => range(1, 10), 'name' => 'test', 'size' => 3]));
     }
 }
