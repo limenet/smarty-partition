afde9b2bb6171cb5ed511470b648a927a9bd833e
Linus Metzler <linus.metzler@gmail.com>
2015-10-10 10:58:52 +0200

    even more!

--- function.partition.php
+++ function.partition.php
@@ -2,8 +2,8 @@
 /**
  * This function partitions an array into n subarrays which can be used for column layouts.
  *
- * @param  array $params            An array consisting of the keys array, size, and name; name is the name of the output array, size is the number of columns, array is the array to be partitioned
- * @param  Smarty_Internal_Template $template Used by Smarty internally
+ * @param  array                    $params     An array consisting of the keys array, size, and name; name is the name of the output array, size is the number of columns, array is the array to be partitioned
+ * @param  Smarty_Internal_Template $template   Used by Smarty internally
  *
  * @return array Returns the partitoned array
  */
