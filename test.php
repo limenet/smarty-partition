a7b27cdacc8842b3fd3f44726304f3ad0caf3853
Linus Metzler <linus.metzler@gmail.com>
2015-10-10 10:54:51 +0200

    some style fixes

--- test.php
+++ test.php
@@ -4,10 +4,10 @@ require_once 'function.partition.php';
 
 class Test extends PHPUnit_Framework_TestCase
 {
-	public function wrapper($a)
+    public function wrapper($a)
     {
-		return smarty_function_partition($a, null);
-	}
+        return smarty_function_partition($a, null);
+    }
 
     public function testEmpty()
     {
@@ -34,17 +34,17 @@ class Test extends PHPUnit_Framework_TestCase
         $this->assertEquals(['test' => []], $this->wrapper(['array' => null, 'name' => 'test', 'size' => 0]));
     }
 
-	public function testPerfectFit()
+    public function testPerfectFit()
     {
         $this->assertEquals(['test' => [[1,2,3],[4,5,6],[7,8,9]]], $this->wrapper(['array' => range(1, 9), 'name' => 'test', 'size' => 3]));
     }
 
-	public function testPerfectOneShort()
+    public function testPerfectOneShort()
     {
         $this->assertEquals(['test' => [[1,2,3],[4,5,6],[7,8]]], $this->wrapper(['array' => range(1, 8), 'name' => 'test', 'size' => 3]));
     }
 
-	public function testPerfectOneOver()
+    public function testPerfectOneOver()
     {
         $this->assertEquals(['test' => [[1,2,3,4],[5,6,7],[8,9,10]]], $this->wrapper(['array' => range(1, 10), 'name' => 'test', 'size' => 3]));
     }
