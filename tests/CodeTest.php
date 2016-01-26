91b6902a5a54fbbe4b1f2dc9b6af1c959db61653
Linus Metzler <linus.metzler@gmail.com>
2016-01-26 03:08:27 -0500

    Applied fixes from StyleCI

--- tests/CodeTest.php
+++ tests/CodeTest.php
@@ -9,47 +9,47 @@ class CodeTest extends PHPUnit_Framework_TestCase
 
     public function testEmpty()
     {
-        $this->assertEquals([], $this->wrapper(['array' => null, 'name' => 'test', 'size' => 3])['test']);
+        $this->assertSame([], $this->wrapper(['array' => null, 'name' => 'test', 'size' => 3])['test']);
     }
 
     public function testOneElement()
     {
-        $this->assertEquals([[1], [], []], $this->wrapper(['array' => [1], 'name' => 'test', 'size' => 3])['test']);
+        $this->assertSame([[1], [], []], $this->wrapper(['array' => [1], 'name' => 'test', 'size' => 3])['test']);
     }
 
     public function testOneColumn()
     {
-        $this->assertEquals([[1]], $this->wrapper(['array' => [1], 'name' => 'test', 'size' => 1])['test']);
+        $this->assertSame([[1]], $this->wrapper(['array' => [1], 'name' => 'test', 'size' => 1])['test']);
     }
 
     public function testOneColumnEmpty()
     {
-        $this->assertEquals([[]], $this->wrapper(['array' => [], 'name' => 'test', 'size' => 1])['test']);
+        $this->assertSame([[]], $this->wrapper(['array' => [], 'name' => 'test', 'size' => 1])['test']);
     }
 
     public function testZeroColumns()
     {
-        $this->assertEquals([], $this->wrapper(['array' => null, 'name' => 'test', 'size' => 0])['test']);
+        $this->assertSame([], $this->wrapper(['array' => null, 'name' => 'test', 'size' => 0])['test']);
     }
 
     public function testNegativeSize()
     {
-        $this->assertEquals([], $this->wrapper(['array' => range(1, 9), 'name' => 'test', 'size' => -3])['test']);
+        $this->assertSame([], $this->wrapper(['array' => range(1, 9), 'name' => 'test', 'size' => -3])['test']);
     }
 
     public function testPerfectFit()
     {
-        $this->assertEquals([[1, 2, 3], [4, 5, 6], [7, 8, 9]], $this->wrapper(['array' => range(1, 9), 'name' => 'test', 'size' => 3])['test']);
+        $this->assertSame([[1, 2, 3], [4, 5, 6], [7, 8, 9]], $this->wrapper(['array' => range(1, 9), 'name' => 'test', 'size' => 3])['test']);
     }
 
     public function testOneShort()
     {
-        $this->assertEquals([[1, 2, 3], [4, 5, 6], [7, 8]], $this->wrapper(['array' => range(1, 8), 'name' => 'test', 'size' => 3])['test']);
+        $this->assertSame([[1, 2, 3], [4, 5, 6], [7, 8]], $this->wrapper(['array' => range(1, 8), 'name' => 'test', 'size' => 3])['test']);
     }
 
     public function testOneOver()
     {
-        $this->assertEquals([[1, 2, 3, 4], [5, 6, 7], [8, 9, 10]], $this->wrapper(['array' => range(1, 10), 'name' => 'test', 'size' => 3])['test']);
+        $this->assertSame([[1, 2, 3, 4], [5, 6, 7], [8, 9, 10]], $this->wrapper(['array' => range(1, 10), 'name' => 'test', 'size' => 3])['test']);
     }
 
     public function testMissingArrayParameter()
