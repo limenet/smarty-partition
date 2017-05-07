d76bfe8f851a2554165cc0f0484986e536c59eb4
Linus Metzler <linus.metzler@gmail.com>
2017-05-07 10:01:14 +0200

    yay for types

--- tests/SmartyTest.php
+++ tests/SmartyTest.php
@@ -42,7 +42,7 @@ class SmartyTest extends TestCase
         $this->htmlOutput(range(1, 13), '<ul><li>1</li><li>2</li><li>3</li><li>4</li></ul><ul><li>5</li><li>6</li><li>7</li></ul><ul><li>8</li><li>9</li><li>10</li></ul><ul><li>11</li><li>12</li><li>13</li></ul>');
     }
 
-    protected function htmlOutput($input, $output)
+    protected function htmlOutput(?array $input, string $output)
     {
         $testFile = __DIR__.'/test.tpl';
         $this->smarty->assign('oneToNine', $input);
